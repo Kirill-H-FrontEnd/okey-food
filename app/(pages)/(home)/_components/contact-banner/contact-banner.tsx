56a54d88f22ecf5c797d309080d0a6a17b897e30
Kirill Horushevskiy <kh.devmain@gmail.com>
2026-01-13 22:04:42 +0200

    fix: remove Silk component and related files for cleanup and update Next.js version

--- app/(pages)/(home)/_components/contact-banner/contact-banner.tsx
+++ app/(pages)/(home)/_components/contact-banner/contact-banner.tsx
@@ -2,8 +2,6 @@ import { FC } from "react";
 import { Container } from "@/components/ui/container";
 import { ArticleBanner } from "./_components/article-banner";
 import { FormBanner } from "./_components/form-banner";
-import dynamic from "next/dynamic";
-// import { SilkClient } from "@/components/ui/silk-client";
 
 type TContactBanner = {};
 
@@ -14,7 +12,6 @@ export const ContactBanner: FC = ({}) => {
         <section className="text-center bg-greenPrimary py-14 px-6 rounded-[16px]">
           <ArticleBanner />
           <FormBanner />
-          {/* <SilkClient /> */}
         </section>
       </Container>
     </section>
