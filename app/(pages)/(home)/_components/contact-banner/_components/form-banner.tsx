f4e868f1d5bb354028af20fa57a5812de2e4a3b8
Kirill Horushevskiy <kh.devmain@gmail.com>
2026-01-17 00:55:02 +0200

    fix: enhance form banner layout and functionality with improved input handling and switch component

--- app/(pages)/(home)/_components/contact-banner/_components/form-banner.tsx
+++ app/(pages)/(home)/_components/contact-banner/_components/form-banner.tsx
@@ -1,28 +1,50 @@
-import { FC } from "react";
+import React, { FC } from "react";
 import s from "./styles/form-banner.module.scss";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
+import Link from "next/link";
 
 type TFormBanner = {};
 
 export const FormBanner: FC = ({}) => {
+  const [accepted, setAccepted] = React.useState(false);
   return (
-    <form className="max-w-[400px] gap-4 m-auto grid mt-6">
-      <div className=" flex gap-2 text-black">
+    <form className="max-w-[450px] gap-4 m-auto grid mt-6">
+      <div className="grid md:flex gap-2 text-black">
+        <Input name="name" placeholder="Имя" className="bg-white  h-[40px]" />
         <Input
-          name="name"
-          placeholder="ваше имя"
-          className="bg-white capitalize h-[40px]"
+          placeholder="Номер телефона"
+          name="phone"
+          type="number"
+          className="bg-white h-[40px]"
         />
-        <Input name="phone" type="number" className="bg-white h-[40px]" />
       </div>
+      <div className="flex items-center justify-start gap-2 text-greySecondary">
+        <Switch
+          checked={accepted}
+          onCheckedChange={setAccepted}
+          className="mt-0.5"
+        />
+        <p className="text-[12px] leading-snug block text-left">
+          Ознакомьтесь с{" "}
+          <Link
+            href={""}
+            className="text-yellow-hover hover:opacity-80 md:text-left md:inline-block"
+          >
+            политикой конфиденциальности.
+          </Link>
+        </p>
+      </div>
+
       <Button
-        className="bg-yellowPrimary text-greenPrimary font-bold w-full py-6"
+        disabled={!accepted}
+        className="bg-yellowPrimary text-greenPrimary font-bold w-full py-6 disabled:opacity-50 disabled:cursor-not-allowed"
         variant="default"
       >
         Связаться с нами
       </Button>
-      <p className="text-greySecondary">
+      <p className="text-greySecondary text-sm ">
         Обычно мы перезваниваем в течение 15 минут
       </p>
     </form>
